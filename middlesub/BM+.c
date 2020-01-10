04f0d587428caeb9f6152042a5d0b25ef6e46dab
udemegane <cappuccino1204.ck@gmail.com>
2020-01-10 13:17:48 +0900

    Update BM+.c

--- middlesub/BM+.c
+++ middlesub/BM+.c
@@ -14,9 +14,11 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
     /*----------------------initialize-------------------------*/
     int t_id, esc, comp_var, point, t_len = strlen(t_in->str);
     string_s *s_i;
+    int tmp_s_len = (&s[0])->len, tmp_i = 0, ifalg = 0, ifirst = 0;
     /*---------------------------------------------------------*/
     for (int i = 0; i < to; i++)
     {
+        tmp_i = i;
         s_i = &s[i];
         //t_id:s_iの最後尾がいる場所
         t_id = s_i->len - 1;
@@ -29,7 +31,7 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
         int table[110][4];
 
         /*---------------------makeShiftTable----------------------*/
-        for (int k = 0; k < s_i->len- 1; k++)
+        for (int k = 0; k < s_i->len - 1; k++)
         {
             int Tcounter;
             rep(j, 120)
@@ -102,7 +104,7 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
                     point = t_id;
                 }
                 /*----------------------DebugPrint-------------------------*/
-/*
+                /*
             printf("t_s:");
             int j;
             for(j=0;j<strlen(t_in->str);j++)
@@ -147,7 +149,7 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
             //マッチした場合t_outに挿入,そうでないなら次の場所を探索
             if (flag == 1)
             {
-                if (s_i->len > 24)
+                if (s_i->len > 50)
                 {
                     int x, y;
                     for (x = s_i->len - 1, y = 0; x >= 0; x--, y++)
@@ -161,12 +163,50 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
                 }
                 else
                 {
+                    /*
+                    if(ifirst>0){
+                        ifirst++;
+                        //printf("\n");
+                        printf("%d回目 len:%d i:%d t_id:%d\n",ifirst,tmp_s_len,i,t_id);
+                        printf(" %s\n",s_i->str);
+                        for(int j=t_id-tmp_s_len;j<t_id+tmp_s_len;j++){
+                            printf("%c",t_in->str[j]);
+                        }
+                        printf("\n");
+                        for(int j=t_id-tmp_s_len;j<t_id+tmp_s_len;j++){
+                            if(j==t_id){
+                                printf("^");
+                            }else printf(" ");
+                        }
+                        printf("\n");
+                    }else {
+                        printf("1回目 len:%d i:%d t_id:%d\n",tmp_s_len,i,t_id);
+                        printf(" %s\n",s_i->str);
+                        ifirst=1;
+                        for(int j=t_id-tmp_s_len;j<t_id+tmp_s_len;j++){
+                            printf("%c",t_in->str[j]);
+                        }
+                        printf("\n");
+                        for(int j=t_id-tmp_s_len;j<t_id+tmp_s_len;j++){
+                            if(j==t_id){
+                                printf("^");
+                            }else printf(" ");
+                        }
+                        printf("\n");
+                    }
+*/
+                    if (tmp_s_len != s_i->len)
+                    {
+                        tmp_s_len = s_i->len;
+                        printf("len:%d\n", tmp_s_len);
+                    }
 
                     linked_push_int(&s_count[s_i->id], t_id - s_i->len + 1, 0);
                     for (int j = 0; j < s_i->len; j++)
                     {
-                        linked_push_node(&t_opt[t_id - s_i->len+ 1 + j], s_i->id, j, 0);
+                        linked_push_node(&t_opt[t_id - s_i->len + 1 + j], s_i->id, j, 0);
                     }
+                    t_id += s_i->len;
                 }
             }
             int temp = High(table[t_id - point + 1][t_in->str[point] - 'a'] + 1,
@@ -175,7 +215,9 @@ void BM(string_out *t_in, string_s *s, int to, string_out *t_out, linked_list *s
             t_id += temp;
             comp_var = 0;
             point = t_id;
+            esc = 0;
         }
-        next:;
+    next:;
+        ifirst = 0;
     }
 }
